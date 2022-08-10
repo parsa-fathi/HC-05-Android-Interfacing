cf84763d996f3e3b1b007eed584c472d540b300b
parsa-fathi <fathi.parsa@outlook.com>
2022-08-10 12:36:49 +0430

    Login page redesigned-icon changed-orientation fixed

--- app/src/main/java/infoaryan/in/hc05_bluetooth/SignIn.java
+++ app/src/main/java/infoaryan/in/hc05_bluetooth/SignIn.java
@@ -3,25 +3,25 @@ package infoaryan.in.hc05_bluetooth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
-
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.TypefaceProvider;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 public class SignIn extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TypefaceProvider.registerDefaultIconSets();
         setContentView(R.layout.login_page);
 
-        final TextView Username = (TextView) findViewById(R.id.Username);
-        final TextView Password = (TextView) findViewById(R.id.Password);
+        final BootstrapEditText Username = (BootstrapEditText) findViewById(R.id.Username);
+        final BootstrapEditText Password = (BootstrapEditText) findViewById(R.id.Password);
 
-        Button LogIn = (Button) findViewById(R.id.btnlogin);
+        BootstrapButton LogIn = (BootstrapButton) findViewById(R.id.btnlogin);
 
         LogIn.setOnClickListener(new View.OnClickListener() {
             @Override
